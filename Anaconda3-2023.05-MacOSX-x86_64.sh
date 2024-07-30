c401165e74a6cb6ec9a4bfdbfdac869861cb3fe4
shivam saini <shivamsaini@shivams-MacBook-Air-2.local>
2024-07-30 16:42:27 +0530

    requirements added

---
+++ Anaconda3-2023.05-MacOSX-x86_64.sh
@@ -0,0 +1,14 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<h1>404 Not Found</h1>
+<ul>
+<li>Code: NoSuchKey</li>
+<li>Message: The specified key does not exist.</li>
+<li>Key: archive/Anaconda3-2023.05-MacOSX-x86_64.sh</li>
+<li>RequestId: 5J4P60V98Y32PZ3Q</li>
+<li>HostId: sR93mxMoA7E8xOtkdFWaOVnCjDQx9hU2/pBk2w9t6vMnFDv9as092ZOhw2iPMi5YpdzPLH4m6aA=</li>
+</ul>
+<hr/>
+</body>
+</html>
